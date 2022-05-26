425a2cfc4f46a197f7bc4a1ea7e6cb2e055c2ea0
nitinmore <nitinmore111@gmail.com>
2022-05-26 14:58:35 +0530

    main class changes

--- src/main/java/in/nareshit/raghu/SpringBootFirstExApplication.java
+++ src/main/java/in/nareshit/raghu/SpringBootFirstExApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootFirstExApplication {
 
 	public static void main(String[] args) {
+		System.out.println("this statement from the main spring boot application");
 		SpringApplication.run(SpringBootFirstExApplication.class, args);
 	}
 
